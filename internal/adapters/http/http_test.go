0346100d3e7a07967766f4062868291087d56dae
Shayan <r3d.l1fe6@gmail.com>
2023-08-17 19:40:36 +0330

    more test cases added

--- internal/adapters/http/http_test.go
+++ internal/adapters/http/http_test.go
@@ -1,9 +1,13 @@
 package http
 
 import (
+	"encoding/json"
 	"github.com/gin-gonic/gin"
 	"github.com/red-life/ipinfo/internal/mocks"
+	"github.com/red-life/ipinfo/internal/ports"
 	"github.com/stretchr/testify/assert"
+	"io"
+	"net"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -20,4 +24,16 @@ func TestIPInfoHandler_Info(t *testing.T) {
 	engine.ServeHTTP(w, req)
 	assert.Equal(t, http.StatusBadRequest, w.Code, "Expected status code to be OK because remote IP is 127.0.0.1")
 
+	ipInfoService.EXPECT().GetInfo(net.ParseIP("185.143.233.200")).Return(ports.Info{}, nil)
+	w = httptest.NewRecorder()
+	req, _ = http.NewRequest("GET", "/185.143.233.200", nil)
+	engine.ServeHTTP(w, req)
+	response, _ := io.ReadAll(w.Body)
+	var jsonResponse map[string]any
+	err := json.Unmarshal(response, &jsonResponse)
+	if err != nil {
+		t.Fatalf("Failed to unmarshal response: %s", err.Error())
+	}
+	_, exists := jsonResponse["error"]
+	assert.Equal(t, false, exists, "Expected to has no errors")
 }
