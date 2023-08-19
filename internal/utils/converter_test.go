043d101537fed364dd0fd629a3e780f86a0330c6
Shayan <r3d.l1fe6@gmail.com>
2023-08-19 16:57:48 +0330

    unit tests added

---
+++ internal/utils/converter_test.go
@@ -0,0 +1,31 @@
+package utils
+
+import (
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestStringToBool(t *testing.T) {
+	testCases := []struct {
+		value    string
+		expected bool
+	}{
+		{
+			value:    "true",
+			expected: true,
+		},
+		{
+			value:    "1",
+			expected: true,
+		},
+		{
+			value:    "tRuE",
+			expected: true,
+		},
+	}
+	for _, testCase := range testCases {
+		desc := fmt.Sprintf("Expected %s to be %v", testCase.value, testCase.expected)
+		assert.Equal(t, testCase.expected, StringToBool(testCase.value), desc)
+	}
+}
